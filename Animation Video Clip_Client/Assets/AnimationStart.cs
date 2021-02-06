cb9aca467237fbc78bc15dd057942242e37763d5
Arbain <arbainrahat@gmail.com>
2021-02-06 23:02:21 +0500

    add

---
+++ Animation Video Clip_Client/Assets/AnimationStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStart : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public void AnimStart()
+    {
+        rb.useGravity = enabled;
+    }
+}
